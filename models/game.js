10cf26636c25a12931b197a73030ddf9a851536f
Keir Innes <keirinnes@Keirs-MacBook-Pro.local>
2024-06-25 06:56:10 +1200

    initial commit

---
+++ models/game.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const GameSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  genre: [{ type: Schema.Types.ObjectId, ref: "Genre" }],
+  price: { type: Number, required: true },
+  numInStock: [{ type: Number, required: true }],
+});
+
+// Virtual for book's URL
+BookSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/catalog/game/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("Game", GameSchema);
